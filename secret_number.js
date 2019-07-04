53c63eb3d2b3a7409196649593374b6a8f0df08b
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-07-03 16:43:20 -1000

    complete

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,9 @@
 'use strict';
 module.exports = function() {
-  
+    let num = Math.floor(Math.random() * 1000001);
+    function secretNumber(){
+        
+        return num;
+    };
+    return secretNumber;
 };
